524766e90a79a7d13dcc8916fd3e78c17463e43a
ALESSANDRO PEREIRA LIMA FILHO <23.01172-6@maua.br>
2023-10-16 11:03:03 -0300

    Melhoria das validações

--- Django/autentificacao_autorizacao/autenticacao_autorizacao_app/views/user_views.py
+++ Django/autentificacao_autorizacao/autenticacao_autorizacao_app/views/user_views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.models import User
 
 class CadastroNovoUsuarioView(views.APIView):
     def post(self, request):
+        print(request)
+        print(request.data)
         serializer = UserSerializers(data=request.data)
         if serializer.is_valid():
-            user = User.objects.create_user(**serializer.validated_data)
-            return Response(UserSerializers(user).data, status=status.HTTP_201_CREATED)
+            user = User.objects.create_user(**serializer.validated_data) #por ser um dicionário nós colocamos dois asteriscos
+            return Response(UserSerializers(user).data, status=status.HTTP_201_CREATED) #boa prática devolver os dados do usuario via objeto
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
                         
