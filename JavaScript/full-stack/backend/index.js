1926d56a5c7c186eea65a0ffe21986933d5dddfd
alexZ7000 <alessandrolimafilho@gmail.com>
2023-11-06 13:00:40 -0300

    Conexão com o mongoDB

--- JavaScript/full-stack/backend/index.js
+++ JavaScript/full-stack/backend/index.js
@@ -1,33 +1,47 @@
+require("dotenv").config();
+const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
+const mongoose = require('mongoose'); //pacote que abstrai as requisições pro mongo(programar menos)
+const mongoConnection = process.env.DATABASE_DEFAULT_CONNECTION;
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-let filmes = [
-    {
-        titulo: "Forrest Gump - O Contador de Histórias",
-        sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções."
-    },
-    {
-        titulo: "Um Sonho de Liberdade",
-        sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-    }
-];
+const Filme = mongoose.model('Filme', mongoose.Schema({
+        titulo: {type: String},
+        sinopse: {type: String}
+    })
+);
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoConnection);
+    console.log('Connected to MongoDB...');
+}
 
 //ponto de acesso para req get teste
-app.get('/filmes', (req, res) => {
+app.get('/filmes', async (req, res) => {
+    const filmes = await Filme.find();
     res.send(filmes);
 });
 
-app.post('/filmes', (req, res) => {
+app.post('/filmes', async (req, res) => {
     const titulo = req.body.titulo;
     const sinopse = req.body.sinopse;
-    const filme = { titulo: titulo, sinopse: sinopse };
-    filmes.push(filme);
-    res.send(filmes);
+    const filme = new Filme({titulo, sinopse});
+    await filme.save();
+    const filmes = await Filme.find();
+    res.json(filmes);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.listen(3000, () => {
+        try{
+            connectToMongo();
+            console.log('Listening on port 3000...');
+        } catch (e) {
+            console.log('Error on port 3000... ', e);
+        }
+    }
+);
 
