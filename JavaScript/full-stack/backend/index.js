9f55adf2372715e2e84256ae6945192f084cd63f
alexZ7000 <alessandrolimafilho@gmail.com>
2023-11-07 09:24:33 -0300

    Correção de .env, criação de uma nova rota, com atualização do package-lock.json mudando a versão do mongoose para a versão 7.0.0 para não dar conflitos com outro pacote mongoose-unique-validator, e começo de criptografia de senhas

--- JavaScript/full-stack/backend/index.js
+++ JavaScript/full-stack/backend/index.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+require("dotenv").config({path: "..\\JavaScript\\full-stack\\.env" });
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose'); //pacote que abstrai as requisições pro mongo(programar menos)
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
 const mongoConnection = process.env.DATABASE_DEFAULT_CONNECTION;
 const app = express();
 
@@ -14,6 +16,14 @@ const Filme = mongoose.model('Filme', mongoose.Schema({
         sinopse: {type: String}
     })
 );
+
+const userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+userSchema.plugin(uniqueValidator);
+const User = mongoose.model('User', userSchema);
 
 const connectToMongo = async () => {
     await mongoose.connect(mongoConnection);
@@ -33,6 +43,21 @@ app.post('/filmes', async (req, res) => {
     await filme.save();
     const filmes = await Filme.find();
     res.json(filmes);
+});
+
+app.get('/login', async (req, res) => {
+    const users = await User.find();
+    res.send(users);
+});
+
+app.post('/login', async (req, res) => {
+    const user = req.body.email;
+    const password = req.body.password;
+    const encryptPassword = await bcrypt.hash(password, 10); //quanto MAIOR o número mais difícil de quebrar e mais demorado para gerar, quanto MENOR mais fácil de quebrar e mais difícil de gerar
+    const users = new User({email: user, password: encryptPassword});
+    const mongoResponse = await users.save()
+    console.log(mongoResponse);
+    res.end();
 });
 
 app.listen(3000, () => {
