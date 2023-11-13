eadcc259d0ac030d7cc7826baeb8d42e33b3ee30
ALESSANDRO PEREIRA LIMA FILHO <23.01172-6@maua.br>
2023-11-13 11:07:04 -0300

    Remoção da instancia global de bloc, criação de um provider

--- Flutter/gerenciamento_de_estados/lib/src/app.dart
+++ Flutter/gerenciamento_de_estados/lib/src/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciamento_de_estados/src/blocs/provider.dart';
 import './telas/login_tela.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'login',
-      home: Scaffold(body: LoginTela(),),
+    return Provider(
+        child: MaterialApp(
+          title: 'login',
+          home: Scaffold(
+            body: LoginTela()
+          )
+        )
     );
   }
 }
